4eae2d0662fb40ced8bc4fcecef2e689344f08e1
Preethi <92843615+Preethi-L@users.noreply.github.com>
2023-09-03 01:48:46 +0530

    Add files via upload

---
+++ E-Shopee store/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Shopee Store. All Rights Reserved  |  Contact Us: +91 12345 67890</p>	
+        </center>
+    </div>
+</footer>
